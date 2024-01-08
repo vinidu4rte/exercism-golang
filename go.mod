df18a6197874dd18973da3353ece2d446cc5a8b0
Vinicius Duarte <viniaduarrte@gmail.com>
2024-01-08 10:19:38 -0300

    init exercismGolang module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module exercismGolang
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"exercismGolang/blackjack"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(blackjack.FirstTurn("ten", "five", "king"))
+}
